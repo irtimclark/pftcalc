ce6ee4c040809ad13ad1b0ca693432a4d1d8e0ee
Tim Clark <tim.clark@inforeliance.com>
2017-03-14 13:22:24 -0400

    removed dead test

--- test/spec/directives/jqmask.js
+++ test/spec/directives/jqmask.js
@@ -5,16 +5,9 @@ describe('Directive: jqmask', function () {
   // load the directive's module
   beforeEach(module('pftcalcApp'));
 
-  var element,
-    scope;
+  var scope;
 
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
-  }));
-
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<jqmask></jqmask>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the jqmask directive');
   }));
 });
